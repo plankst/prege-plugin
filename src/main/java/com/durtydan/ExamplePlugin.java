358c5634a9a3b06c69fae29aa65bb4933a4a9f2e
plankst <rodanny121@gmail.com>
2023-05-28 16:35:56 -0400

    Made Pre-GE plugin

---
+++ src/main/java/com/durtydan/ExamplePlugin.java
@@ -0,0 +1,92 @@
+package com.durtydan;
+
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.Client;
+import net.runelite.api.MenuEntry;
+import net.runelite.api.events.MenuOptionClicked;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.chat.ChatMessageManager;
+import net.runelite.client.chat.QueuedMessage;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.util.Text;
+
+//
+@Slf4j
+@PluginDescriptor(
+	name = "Pre-GE"
+)
+public class ExamplePlugin extends Plugin
+{
+	@Inject
+	private Client client;
+
+	@Inject
+	private ChatMessageManager chatMessageManager;
+
+	@Inject
+	private ClientThread clientThread;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		log.info("Pre-GE started!");
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		log.info("Pre-GE stopped!");
+	}
+
+	@Subscribe
+	public void onMenuOptionClicked(MenuOptionClicked event)
+	{
+		String target = Text.removeTags(event.getMenuTarget());
+
+		if (entryMatches(event, "Exchange", "Grand Exchange Clerk")
+				|| entryMatches(event, "Exchange", "Grand Exchange booth")
+				|| entryMatches(event, "Collect", "Banker")
+				|| entryMatches(event, "Collect", "Grand Exchange booth")
+				|| entryMatches(event, "Collect", "Bank booth"))
+		{
+			// Send Message to player
+			event.consume();
+			sendChatMessage("Do what? It doesn't exist...");
+			return;
+		}
+
+	}
+	private boolean entryMatches(MenuEntry entry, String option)
+	{
+		return entry.getOption().equals(option);
+	}
+
+	private boolean entryMatches(MenuOptionClicked event, String option)
+	{
+		return event.getMenuOption().equals(option);
+	}
+
+	private boolean entryMatches(MenuEntry entry, String option, String target)
+	{
+		return entryMatches(entry, option) && Text.removeTags(entry.getTarget()).equals(target);
+	}
+
+	private boolean entryMatches(MenuOptionClicked event, String option, String target)
+	{
+		return entryMatches(event, option) && Text.removeTags(event.getMenuTarget()).equals(target);
+	}
+
+	private void sendChatMessage(String message)
+	{
+		chatMessageManager.queue(QueuedMessage.builder()
+				.type(ChatMessageType.CONSOLE)
+				.runeLiteFormattedMessage(message)
+				.build());
+	}
+
+
+}
--- src/test/java/com/durtydan/preGE.java
+++ src/test/java/com/durtydan/preGE.java
@@ -1,9 +1,9 @@
-package com.example;
+package com.durtydan;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class preGE
 {
 	public static void main(String[] args) throws Exception
 	{
